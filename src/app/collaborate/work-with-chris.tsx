72f9b40b42adbe544795b729ba2f3e054109f345
Muchiri <developedbyrobbie@gmail.com>
2024-08-22 09:33:25 +0300

    feat: add landing on service page

--- src/app/collaborate/work-with-chris.tsx
+++ src/app/collaborate/work-with-chris.tsx
@@ -1,8 +1,15 @@
+import Landing from "../_components/landing"
 
 
 const WorkWithChris = () => {
   return (
-    <div className="min-h-[100vh]">WorkWithChris</div>
+    <div className="min-h-[100vh]">
+      <Landing
+        imgSrc={"https://utfs.io/f/f6489a9d-320c-4187-ad58-bca5f644e03c-ucw8jt.jpg"}
+        title={"Collaborate"}
+        subtitle={"Partner with Sprouting Roots Consultancy to cultivate thriving, sustainable gardens. Explore our tailored services and collaborative opportunities, designed to empower individuals, schools, and communities in their gardening journey."}
+      />
+    </div>
   )
 }
 
