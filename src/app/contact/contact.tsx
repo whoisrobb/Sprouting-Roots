28a01856b2c0f79579a6411a3bc243e83d3384b2
Muchiri <developedbyrobbie@gmail.com>
2024-08-22 11:43:48 +0300

    feat: add landing on contact page

--- src/app/contact/contact.tsx
+++ src/app/contact/contact.tsx
@@ -1,8 +1,15 @@
+import Landing from "../_components/landing"
 
 
 const Contact = () => {
   return (
-    <div className="min-h-[100vh]">Contact</div>
+    <div className="min-h-[100vh]">
+      <Landing
+        imgSrc={"https://utfs.io/f/25334bfe-b309-466d-9ef3-6472f29e499d-pu41nf.jpg"}
+        title={"Get in touch"}
+        subtitle={"Reach out to Sprouting Roots Consultancy for personalized gardening support. We're here to answer your questions, provide guidance, and help you grow a thriving, sustainable garden."}
+      />
+    </div>
   )
 }
 
